104ecac2bc541217a6b0c28922a78d7ff05a3f97
stodorichkov <steliantodorichkov03@gmail.com>
2024-08-10 10:01:04 +0300

    [SPB-1] Initialize Eureka Spring Boot project

---
+++ eureka/src/test/java/com/heim/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heim.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
